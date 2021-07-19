2eee644c3a65e9a4d78812419d12a2ead2ae3a92
tyleraguirre <taguirre6807@xmail.dixietech.edu>
2021-07-19 16:05:05 -0600

    added api and fixed gradle file

--- app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/com/example/android/marsrealestate/network/MarsApiService.kt
@@ -1,4 +1,4 @@
-/*
+ /*
  * Copyright 2018, The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -15,6 +15,30 @@
  *
  */
 
-package com.example.android.marsrealestate.network
+ package com.example.android.marsrealestate.network
 
-private const val BASE_URL = "https://mars.udacity.com/"
+
+ import retrofit2.Call
+ import retrofit2.Retrofit
+ import retrofit2.converter.scalars.ScalarsConverterFactory
+ import retrofit2.http.GET
+
+
+ private const val BASE_URL = "https://mars.udacity.com/"
+
+ private val retrofit = Retrofit.Builder()
+     .addConverterFactory(ScalarsConverterFactory.create())
+     .baseUrl(BASE_URL)
+     .build()
+
+ interface MarsApiService {
+     @GET("realestate")
+     fun getProperties():
+             Call<String>
+ }
+
+ object MarsApi {
+     val retrofitService : MarsApiService by lazy {
+         retrofit.create(MarsApiService::class.java)
+     }
+ }
